dfd6100b550698e47f5f16ae179314e7efa71856
shariar-007 <uiu.151007@gmail.com>
2019-11-20 01:52:29 +0600

    Express Server With Static Files

--- NODE-EXPRESS/index.js
+++ NODE-EXPRESS/index.js
@@ -1,21 +1,48 @@
- express = require('express'),
-     http = require('http');
+//  express = require('express'),
+//  http = require('http');
 
- hostname = 'localhost';
- port = 3000;
+//  hostname = 'localhost';
+//  port = 3000;
 
- app = express();
+//  app = express();
 
- app.use((req, res, next) => {
-     console.log(req.headers);
-     res.statusCode = 200;
-     res.setHeader('Content-Type', 'text/html');
-     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+//  app.use((req, res, next) => {
+//      console.log(req.headers);
+//      res.statusCode = 200;
+//      res.setHeader('Content-Type', 'text/html');
+//      res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
- });
+//  });
 
- server = http.createServer(app);
+//  server = http.createServer(app);
 
- server.listen(port, hostname, () => {
-     console.log(`Server running at http://${hostname}:${port}/`);
- });
+//  server.listen(port, hostname, () => {
+//      console.log(`Server running at http://${hostname}:${port}/`);
+//  });
+
+//-----------------------------------------------------------------------------------------
+
+express = require('express'),
+    http = require('http');
+morgan = require('morgan');
+
+hostname = 'localhost';
+port = 3000;
+
+app = express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    // console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
+});
+
+server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
