1f07d6f4a58a7a4319997d3a606fee4f3e779cb1
shariar-007 <uiu.151007@gmail.com>
2019-11-24 03:18:01 +0600

    Node Callback Hell and Promises

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -40,32 +40,72 @@ assert = require('assert');
 url = 'mongodb://localhost:27017/';
 dbname = 'conFusion';
 dboper = require('./operations');
-MongoClient.connect(url, (err, client) => {
-    assert.equal(err, null);
-    console.log('Connected correctly to server');
 
-    db = client.db(dbname);
-    dboper.insertDocument(db, { name: "Vadonut", description: "Test" }, "dishes", (result) => {
-        console.log("Insert Document:\n", result.ops);
+//===== this is callback method way to do operation ====
+// MongoClient.connect(url, (err, client) => {
+//     assert.equal(err, null);
+//     console.log('Connected correctly to server');
 
-        dboper.findDocuments(db, "dishes", (docs) => {
-            console.log("Found Documents:\n", docs);
+//     db = client.db(dbname);
 
-            dboper.updateDocument(db, { name: "Vadonut" }, { description: "Updated Test" }, "dishes",
-                (result) => {
-                    console.log("Updated Document:\n", result.result);
+// dboper.insertDocument(db, { name: "Vadonut", description: "Test" }, "dishes", (result) => {
+//     console.log("Insert Document:\n", result.ops);
 
-                    dboper.findDocuments(db, "dishes", (docs) => {
-                        console.log("Found Updated Documents:\n", docs);
+//     dboper.findDocuments(db, "dishes", (docs) => {
+//         console.log("Found Documents:\n", docs);
 
-                        db.dropCollection("dishes", (result) => {
-                            console.log("Dropped Collection: ", result);
+//         dboper.updateDocument(db, { name: "Vadonut" }, { description: "Updated Test" }, "dishes", (result) => {
+//                 console.log("Updated Document:\n", result.result);
 
-                            client.close();
-                        });
-                    });
-                });
-        });
-    });
+//                 dboper.findDocuments(db, "dishes", (docs) => {
+//                     console.log("Found Updated Documents:\n", docs);
 
-});
+//                     db.dropCollection("dishes", (result) => {
+//                         console.log("Dropped Collection: ", result);
+
+//                         client.close();
+//                     });
+//                 });
+//          });
+//     });
+// });
+// });
+
+//===== this is promise method way to do operation ====
+MongoClient.connect(url).then((client) => {
+
+        console.log('Connected correctly to server');
+        const db = client.db(dbname);
+
+        dboper.insertDocument(db, { name: "Vadonut", description: "Test" },
+                "dishes")
+            .then((result) => {
+                console.log("Insert Document:\n", result.ops);
+
+                return dboper.findDocuments(db, "dishes");
+            })
+            .then((docs) => {
+                console.log("Found Documents:\n", docs);
+
+                return dboper.updateDocument(db, { name: "Vadonut" }, { description: "Updated Test" }, "dishes");
+
+            })
+            .then((result) => {
+                console.log("Updated Document:\n", result.result);
+
+                return dboper.findDocuments(db, "dishes");
+            })
+            .then((docs) => {
+                console.log("Found Updated Documents:\n", docs);
+
+                return db.dropCollection("dishes");
+            })
+            .then((result) => {
+                console.log("Dropped Collection: ", result);
+
+                return client.close();
+            })
+            .catch((err) => console.log(err));
+
+    })
+    .catch((err) => console.log(err));
