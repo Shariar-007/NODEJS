cf06b036d227054cd2a8165b1a117d85e082fae2
shariar-007 <uiu.151007@gmail.com>
2019-11-25 02:58:46 +0600

    Mongoose part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -9,6 +9,8 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
+    // ===>> Mongoose Part 1
+
     // var newDish = Dishes({
     //     name: 'Uthappizza',
     //     description: 'test'
@@ -31,17 +33,53 @@ connect.then((db) => {
     //         console.log(err);
     //     });
 
-    // another way
+    // ===>> Mongoose Part 2
+    // Dishes.create({
+    //         name: 'Uthappizza',
+    //         description: 'test'
+    //     })
+    //     .then((dish) => {
+    //         console.log(dish);
+    //         return Dishes.find({}).exec();
+    //     })
+    //     .then((dishes) => {
+    //         console.log(dishes);
+    //         return Dishes.remove({});
+    //     })
+    //     .then(() => {
+    //         return mongoose.connection.close();
+    //     })
+    //     .catch((err) => {
+    //         console.log(err);
+    //     });
+
+    // ===>> Mongoose Part 3
     Dishes.create({
             name: 'Uthappizza',
             description: 'test'
         })
         .then((dish) => {
             console.log(dish);
-            return Dishes.find({}).exec();
+
+            return Dishes.findByIdAndUpdate(dish._id, { $set: { description: 'Updated test' } }, {
+                    new: true
+                })
+                .exec();
         })
-        .then((dishes) => {
-            console.log(dishes);
+        .then((dish) => {
+            console.log(dish);
+
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
+
+            return dish.save();
+        })
+        .then((dish) => {
+            console.log(dish);
+
             return Dishes.remove({});
         })
         .then(() => {
@@ -50,5 +88,4 @@ connect.then((db) => {
         .catch((err) => {
             console.log(err);
         });
-
 });
--- node-mongoose/models/dishes.js
+++ node-mongoose/models/dishes.js
@@ -1,6 +1,25 @@
 mongoose = require('mongoose');
 Schema = mongoose.Schema;
 
+var commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
 dishSchema = new Schema({
     name: {
         type: String,
@@ -10,7 +29,8 @@ dishSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema]
 }, {
     timestamps: true
 });
