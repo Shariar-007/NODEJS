686e0235b9d32a82f116a937470cdb07dcebdbe5
shariar-007 <uiu.151007@gmail.com>
2019-12-02 03:26:05 +0600

    Express Session Part 2

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -65,41 +65,61 @@ app.use(session({
 //     }
 // }
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+// function auth(req, res, next) {
+//     console.log(req.session);
+
+//     // if (!req.signedCookies.user) {
+//     if (!req.session.user) {
+//         var authHeader = req.headers.authorization;
+//         if (!authHeader) {
+//             var err = new Error('You are not authenticated!');
+//             res.setHeader('WWW-Authenticate', 'Basic');
+//             err.status = 401;
+//             next(err);
+//             return;
+//         }
+//         var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+//         var user = auth[0];
+//         var pass = auth[1];
+//         if (user == 'admin' && pass == 'password') {
+//             // res.cookie('user', 'admin', { signed: true });
+//             req.session.user = 'admin';
+//             next(); // authorized
+//         } else {
+//             var err = new Error('You are not authenticated!');
+//             res.setHeader('WWW-Authenticate', 'Basic');
+//             err.status = 401;
+//             next(err);
+//         }
+//     } else {
+//         // if (req.signedCookies.user === 'admin') {
+//         if (req.session.user === 'admin') {
+//             console.log('req.session: ', req.session);
+//             next();
+//         } else {
+//             var err = new Error('You are not authenticated!');
+//             err.status = 401;
+//             next(err);
+//         }
+//     }
+// }
+
 function auth(req, res, next) {
     console.log(req.session);
 
-    // if (!req.signedCookies.user) {
     if (!req.session.user) {
-        var authHeader = req.headers.authorization;
-        if (!authHeader) {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            next(err);
-            return;
-        }
-        var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-        var user = auth[0];
-        var pass = auth[1];
-        if (user == 'admin' && pass == 'password') {
-            // res.cookie('user', 'admin', { signed: true });
-            req.session.user = 'admin';
-            next(); // authorized
-        } else {
-            var err = new Error('You are not authenticated!');
-            res.setHeader('WWW-Authenticate', 'Basic');
-            err.status = 401;
-            next(err);
-        }
+        var err = new Error('You are not authenticated!');
+        err.status = 403;
+        return next(err);
     } else {
-        // if (req.signedCookies.user === 'admin') {
-        if (req.session.user === 'admin') {
-            console.log('req.session: ', req.session);
+        if (req.session.user === 'authenticated') {
             next();
         } else {
             var err = new Error('You are not authenticated!');
-            err.status = 401;
-            next(err);
+            err.status = 403;
+            return next(err);
         }
     }
 }
@@ -110,9 +130,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 console.log(path.join(__dirname, 'public'));
 
-// app.use('/', indexRouter);
-app.use('/index', indexRouter);
-app.use('/users', usersRouter);
+
+// app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
