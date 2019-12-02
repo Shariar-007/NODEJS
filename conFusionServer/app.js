9b43138422632606466b18c695dae5b9f5b18250
shariar-007 <uiu.151007@gmail.com>
2019-12-03 03:21:49 +0600

    Passport

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -5,6 +5,8 @@ var path = require('path');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -41,6 +43,9 @@ app.use(session({
     resave: false,
     store: new FileStore()
 }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // function auth(req, res, next) {
 //     console.log(req.headers);
@@ -106,21 +111,33 @@ app.use('/users', usersRouter);
 //     }
 // }
 
-function auth(req, res, next) {
-    console.log(req.session);
+// function auth(req, res, next) {
+//     console.log(req.session);
 
-    if (!req.session.user) {
+//     if (!req.session.user) {
+//         var err = new Error('You are not authenticated!');
+//         err.status = 403;
+//         return next(err);
+//     } else {
+//         if (req.session.user === 'authenticated') {
+//             next();
+//         } else {
+//             var err = new Error('You are not authenticated!');
+//             err.status = 403;
+//             return next(err);
+//         }
+//     }
+// }
+
+function auth(req, res, next) {
+    console.log(req.user);
+
+    if (!req.user) {
         var err = new Error('You are not authenticated!');
         err.status = 403;
         return next(err);
     } else {
-        if (req.session.user === 'authenticated') {
-            next();
-        } else {
-            var err = new Error('You are not authenticated!');
-            err.status = 403;
-            return next(err);
-        }
+        next();
     }
 }
 
