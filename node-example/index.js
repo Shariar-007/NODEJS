e2a603cc24f1be484b12f2fb4745e3d7d63d1f79
shariar-007 <uiu.151007@gmail.com>
2020-04-25 04:26:03 +0600

    again look into node-example to learn the nodejs from the start

--- node-example/index.js
+++ node-example/index.js
@@ -1,5 +1,11 @@
 var rect = require('./rectangle');
 
+
+// var rect = {
+//     perimeter: (x, y) => (2 * (x * y)),
+//     area: (x, y) => (x * y)
+// }
+
 // function solveRect(l, b) {
 //     console.log("Solving For rectangle with l = " + l + " and b = " + b);
 //     if (l <= 0 || b <= 0) {
